addbdc4df672e6270c41125f5d76450b3cc66f92
martin.copyright <martin@copyright.rip>
2025-11-01 14:51:03 +0100

    mobile support
    you know the drill

--- server.js
+++ server.js
@@ -4,19 +4,32 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  maxHttpBufferSize: 1e6, // 1MB max per message
+});
 
-// Serve static files from "public"
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3000;
+
+// Store user cursors
 let users = {};
+
+// Throttle broadcasting to 30 FPS
+let lastBroadcast = 0;
+const BROADCAST_INTERVAL = 33; // ms ~30FPS
 
 io.on('connection', (socket) => {
   users[socket.id] = {};
 
   socket.on('cursor-move', (data) => {
     users[socket.id] = data;
-    io.emit('cursors-update', users);
+
+    const now = Date.now();
+    if (now - lastBroadcast > BROADCAST_INTERVAL) {
+      io.emit('cursors-update', users);
+      lastBroadcast = now;
+    }
   });
 
   socket.on('disconnect', () => {
@@ -25,5 +38,4 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
