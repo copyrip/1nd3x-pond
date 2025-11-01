e74256143ffc844189fcc7cdd61c28a520f3359a
martin.copyright <martin@copyright.rip>
2025-11-01 16:34:12 +0100

    forgot server.js

--- server.js
+++ server.js
@@ -1,79 +1,64 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const WebSocket = require('ws');
 const path = require('path');
 
-const app = express();
+const app = require('./app'); // Import express app
 const server = http.createServer(app);
-const io = new Server(server);
+const wss = new WebSocket.Server({ server });
+
+let users = {}; // {id: {username, color, cursorColor, connectTime, x, y}}
+
+function broadcast(data) {
+    const message = JSON.stringify(data);
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
+wss.on('connection', (ws) => {
+    const id = Date.now() + Math.random(); // unique id
+    const connectTime = Date.now();
+    users[id] = {
+        username: `User${Object.keys(users).length + 1}`,
+        color: '#000000',
+        cursorColor: '#ff0000',
+        connectTime,
+        x: 0,
+        y: 0
+    };
+
+    // Send initial data to client
+    ws.send(JSON.stringify({ type: 'init', id, users }));
+
+    // Broadcast new user count
+    broadcast({ type: 'updateUsers', users });
+
+    ws.on('message', (msg) => {
+        const data = JSON.parse(msg);
+        if (data.type === 'mousemove') {
+            users[id].x = data.x;
+            users[id].y = data.y;
+            broadcast({ type: 'mousemove', id, x: data.x, y: data.y });
+        } else if (data.type === 'updateUsername') {
+            users[id].username = data.username;
+            users[id].color = data.color;
+            broadcast({ type: 'updateUsers', users });
+        } else if (data.type === 'updateCursorColor') {
+            users[id].cursorColor = data.cursorColor;
+            broadcast({ type: 'updateUsers', users });
+        } else if (data.type === 'draw') {
+            broadcast({ type: 'draw', x: data.x, y: data.y, color: users[id].cursorColor });
+        }
+    });
+
+    ws.on('close', () => {
+        delete users[id];
+        broadcast({ type: 'updateUsers', users });
+    });
+});
 
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-let users = {};
-
-io.on('connection', (socket) => {
-    const user = {
-        id: socket.id,
-        username: `User${Math.floor(Math.random() * 1000)}`,
-        cursorColor: '#ff0000',
-        nameColor: '#000000',
-        points: 0,
-        startTime: Date.now()
-    };
-    users[socket.id] = user;
-
-    // Send initial user data
-    socket.emit('init', { id: socket.id, users });
-
-    // Notify others
-    io.emit('updateUsers', users);
-
-    socket.on('cursorMove', (data) => {
-        if(users[socket.id]) {
-            users[socket.id].x = data.x;
-            users[socket.id].y = data.y;
-            io.emit('cursorMove', { id: socket.id, x: data.x, y: data.y });
-        }
-    });
-
-    socket.on('click', () => {
-        if(users[socket.id]) {
-            users[socket.id].points += 1;
-            io.emit('updateUsers', users);
-            io.emit('clickEffect', { id: socket.id });
-        }
-    });
-
-    socket.on('bigClick', () => {
-        if(users[socket.id]) {
-            users[socket.id].points += 10;
-            io.emit('updateUsers', users);
-            io.emit('clickEffect', { id: socket.id });
-        }
-    });
-
-    socket.on('customize', (data) => {
-        if(users[socket.id]) {
-            users[socket.id].cursorColor = data.cursorColor;
-            users[socket.id].nameColor = data.nameColor;
-            io.emit('updateUsers', users);
-        }
-    });
-
-    socket.on('setUsername', (newName) => {
-    if(users[socket.id]) {
-        users[socket.id].username = newName;
-        io.emit('updateUsers', users);
-    }
-});
-
-
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('updateUsers', users);
-    });
-});
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
